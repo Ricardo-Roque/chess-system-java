bf3daf21048fadc5b6197715dd82506c53419201
ricardo.roque <ricardo.roque@neomind.com.br>
2024-07-24 21:05:53 -0300

    ChessException and ChessPosition

--- src/main/java/application/Program.java
+++ src/main/java/application/Program.java
@@ -1,11 +1,12 @@
 package application;
 
 import chess.ChessMatch;
+
 import static application.UI.printBoard;
 
 public class Program {
 
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         ChessMatch chessMatch = new ChessMatch();
 
